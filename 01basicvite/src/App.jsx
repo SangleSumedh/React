59e494ccc77029968088d67d6e743b3cbf146faa
Sumedh Sangle <sanglesumedh15@gmail.com>
2025-01-31 13:56:23 +0530

    Add basic react and basic vite+react application

---
+++ 01basicvite/src/App.jsx
@@ -0,0 +1,13 @@
+import youtube from "./Youtube";
+
+function App() {
+  return (
+    <div>
+      <h1>Create Vite React App</h1>
+      <h1>Create Vite React App</h1>
+      <Youtube />
+    </div>
+  );
+}
+
+export default App;
