342dea004b08f1dd6c8c2cb85749ffb2cc403bf3
Sumedh Sangle <sanglesumedh15@gmail.com>
2025-02-07 02:30:21 +0530

    Add todo list

--- 09themeSwitch/src/App.jsx
+++ 09themeSwitch/src/App.jsx
@@ -25,7 +25,7 @@ function App() {
 
   return (
     <ThemeProvider value={{ themeMode, darkTheme, lightTheme }}>
-      <div className="flex flex-wrap min-h-screen items-center bg-gray-6f00">
+      <div className="flex flex-wrap min-h-screen items-center bg-gray-600">
         <div className="w-full">
           <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
             <ThemeBtn />
